23cbf74cdb3d96bf6c7b66e6c5669e5b5667d2e1
Yilong Liu <liuyilong@momenta.ai>
2018-09-10 11:49:47 +0800

    Added config-zsh

---
+++ scripts/config-zsh.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Install zsh
+sudo apt update
+sudo apt install zsh wget git
+chsh -s $(which zsh)
+
+# Install oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# Install additional plugins
+
+## zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
+## zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+compaudit | xargs chmod g-w,o-w  
+
+## thefuck
+sudo apt install python3-dev python3-pip
+sudo pip3 install thefuck
+
+# Restore .zshrc
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+if [ -f ../data/.zshrc ]; then
+	cp ../data/.zshrc ~/
+	source ~/.zshrc
+fi
